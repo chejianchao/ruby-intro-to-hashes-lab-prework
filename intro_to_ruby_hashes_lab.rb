24be086da508ecac1c5b41d460eed693418fe775
chejianchao <820992376@qq.com>
2018-07-08 05:20:35 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,60 +13,33 @@ end
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-	monopoly[:railroads][:pieces] = 4
-	monopoly[:railroads][:rent_in_dollars] = {}
-	monopoly[:railroads][:names] = {}
-	monopoly
+	obj = monopoly
+	obj[:railroads][:pieces] = 4
+	obj[:railroads][:rent_in_dollars] = {}
+	obj[:railroads][:names] = {}
+	obj
 end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-	  monopoly_with_second_tier[:railroads][:rent_in_dollars][:one_piece_owned] =25
-    monopoly_with_second_tier[:railroads][:rent_in_dollars][:two_pieces_owned] =50
-    monopoly_with_second_tier[:railroads][:rent_in_dollars][:three_pieces_owned] =100
-    monopoly_with_second_tier[:railroads][:rent_in_dollars][:four_pieces_owned] =200
+	  obj = monopoly_with_second_tier
+	  obj[:railroads][:rent_in_dollars][:one_piece_owned] =25
+    obj[:railroads][:rent_in_dollars][:two_pieces_owned] =50
+    obj[:railroads][:rent_in_dollars][:three_pieces_owned] =100
+    obj[:railroads][:rent_in_dollars][:four_pieces_owned] =200
     
-    monopoly_with_second_tier[:railroads][:names][:reading_railroad] = {}
-    monopoly_with_second_tier[:railroads][:names][:pennsylvania_railroad] = {}
-    monopoly_with_second_tier[:railroads][:names][:b_and_o_railroad] = {}
-    monopoly_with_second_tier[:railroads][:names][:shortline] = {}
-
+    obj[:railroads][:names][:reading_railroad] = {}
+    obj[:railroads][:names][:pennsylvania_railroad] = {}
+    obj[:railroads][:names][:b_and_o_railroad] = {}
+    obj[:railroads][:names][:shortline] = {}
+    obj
 end
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-	monopoly =  {
-  :railroads=>
-  {
-    :pieces=>4,
-    :rent_in_dollars=>
-    {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
-    },
-    :names=>
-    {
-      :reading_railroad=>
-      {
-        "mortgage_value"=>"$100"
-      },
-      :pennsylvania_railroad=>
-      {
-        "mortgage_value"=>"$200"
-      },
-      :b_and_o_railroad=>
-      {
-        "mortgage_value"=>"$400"
-      },
-      :shortline=>
-      {
-        "mortgage_value"=>"$800"
-      }
-    }
-  }
-}
+  obj = monopoly_with_third_tier
+  
+  obj
 end
 
 
