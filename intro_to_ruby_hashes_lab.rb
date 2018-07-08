a6ac1ab601f682a36bb54d1e051759fd7a6e4838
chejianchao <820992376@qq.com>
2018-07-08 05:18:34 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,22 +29,8 @@ def monopoly_with_third_tier
     monopoly_with_second_tier[:railroads][:names][:reading_railroad] = {}
     monopoly_with_second_tier[:railroads][:names][:pennsylvania_railroad] = {}
     monopoly_with_second_tier[:railroads][:names][:b_and_o_railroad] = {}
-    monopoly_with_second_tier[:railroads][:names][:b_and_o_railroad] = {}
+    monopoly_with_second_tier[:railroads][:names][:shortline] = {}
 
-      it "sets the 3rd key of the :names hash to a symbol, :b_and_o_railroad, whose value is an empty hash" do
-
-        expect(monopoly_with_third_tier.keys.count).to eq(1)
-        expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
-        expect(monopoly_with_third_tier.values[0][:names].keys[2]).to eq(:b_and_o_railroad)
-        expect(monopoly_with_third_tier.values[0][:names].values[2]).to eq({})
-      end
-
-      it "sets the 4th key of the :names hash to a symbol, :shortline, whose value is an empty hash" do
-
-        expect(monopoly_with_third_tier.keys.count).to eq(1)
-        expect(monopoly_with_third_tier.values[0].values.count).to eq(3)
-        expect(monopoly_with_third_tier.values[0][:names].keys[3]).to eq(:shortline)
-        expect(monopoly_with_third_tier.values[0][:names].values[3]).to eq({})
 end
 
 def monopoly_with_fourth_tier
